c4df7f3ccc5b794724e1d1d33431d971b554af24
JspecGeek4maniacs <juliogouveiagtr@gmail.com>
2023-02-13 10:07:02 +0000

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,22 +1,25 @@
-om django.contrib import admin
-# <HINT> Import any new Models here
+from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner
-from .models import Question, Choice
+from .models import Question, Choice, Submission, Enrollment
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-class ChoiceInline(admin.TabularInline):
+# Removed QuestionInline as it is no longer needed, 
+# because I wanted the questions to be related directly to courses, not to lessons
+# Leaving it here for grading purposes
+# class QuestionInline(admin.StackedInline):
+#     model = Question
+#     extra = 1
+
+
+class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 3
+    extra = 2
 
-class QuestionAdmin(admin.ModelAdmin):
-    model = Question
-    inlines = [ChoiceInline]
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 2
 
-# Register your models here.
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
@@ -25,12 +28,25 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'content']
+    # inlines = [QuestionInline] 
 
 
-# <HINT> Register Question and Choice models here
-admin.site.register(Question, QuestionAdmin)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'course', 'date_enrolled', 'mode', 'rating']
+
+
+# Question and Choice models
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text']
+    inlines = [ChoiceInline]
+
+
+# Registering models for admin site
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Enrollment, EnrollmentAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Submission)
